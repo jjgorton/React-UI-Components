8506bc680c5a3c4636a7a084861bd235c139a19c
jjgorton <jjgorton@gmail.com>
2019-03-18 18:23:28 -0400

    components complete, needs more styling, and footer components for stretch

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+	return (
+		<div className="header-title">
+			<h1>Lambda School</h1>
+			<p>@LambdaSchool</p>
+			<p>
+				<li>26 jan</li>
+			</p>
+		</div>
+	);
+};
+
+export default HeaderTitle;
